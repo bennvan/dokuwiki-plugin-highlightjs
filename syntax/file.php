199a1aa656d8f7f3f560c65a73f4632490775c67
Ben <ben.vanmagill16@gmail.com>
2021-10-08 17:43:08 +1100

    separate file syntax

---
+++ syntax/file.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * DokuWiki Plugin HighlightJS
+ *
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author Ben van Magill <ben.vanmagill16@gmail.com>
+ *
+ * usage: <file html nowrap [downloadfile.html] | Title > ... </file>
+ */
+
+if (!defined('DOKU_INC')) die();
+
+class syntax_plugin_codehighlightjs_file extends syntax_plugin_codehighlightjs_code
+{
+    public function connectTo($mode)
+    {
+        $this->Lexer->addEntryPattern('<file\b(?=.*</file>)', $mode, 'plugin_codehighlightjs_file');
+    }
+
+    public function postConnect()
+    {
+        $this->Lexer->addExitPattern('</file>', 'plugin_codehighlightjs_file');
+    }
+}
